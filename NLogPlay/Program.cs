c5fc279a9de794193a796e516d50fa86e128733a
Marcos Almeida <marcos.almeida@xcomponent.com>
2017-04-17 20:37:27 +0200

    Making example work with ElasticSearch

--- NLogPlay/Program.cs
+++ NLogPlay/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace NLogPlay
 {
     class Program
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            const string name = "marcos";
+            _logger.Info("Hello world! {0}", name);
+            Console.ReadKey();
         }
     }
 }
